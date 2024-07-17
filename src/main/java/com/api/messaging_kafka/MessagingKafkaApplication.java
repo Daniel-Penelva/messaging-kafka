50d8d11568b055a9330954da0d7ad84adc4671db
Daniel Penelva <d4n.andrade@gmail.com>
2024-07-17 03:44:59 -0300

    feat: criando a classe MessagingController Essa classe vai expor uma API REST que envia mensagens para um tópico kafka. Esse método utiliza o HelloProducer para enviar uma mensagem personalizada ao Kafka e retorna "OK" como resposta.

--- src/main/java/com/api/messaging_kafka/MessagingKafkaApplication.java
+++ src/main/java/com/api/messaging_kafka/MessagingKafkaApplication.java
@@ -3,6 +3,12 @@ package com.api.messaging_kafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
 public class MessagingKafkaApplication {
 
@@ -10,4 +16,18 @@ public class MessagingKafkaApplication {
 		SpringApplication.run(MessagingKafkaApplication.class, args);
 	}
 
+}
+
+@RestController
+@RequestMapping("/kafka")
+class MessagingController {
+	@Autowired
+	private HelloProducer service;
+
+	// http://localhost:8080/kafka/hello/Daniel
+	@GetMapping("/hello/{name}")
+	public String hello(@PathVariable("name") String name) {
+		service.sendMessage("Olá, " + name);
+		return "OK";
+	}
 }
